15a1a6f4c1e3ef93f8ef5aeee64c4781540a89f7
usmanjavaid227 <71181962+usmanjavaid227@users.noreply.github.com>
2023-05-04 10:11:36 +0500

    setup the basic structure{template} of the website

---
+++ core/urls.py
@@ -0,0 +1,36 @@
+"""intraviewer URL Configuration
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from core import views
+from django.views.generic import TemplateView
+
+
+urlpatterns = [
+    
+    path('', views.index,name='index'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('feedback/', views.feedback, name='feedback'),
+    path('history/', views.history, name='history'),
+    path('interview/', views.interview, name='interview'),
+    path('services/', views.services, name='services'),
+    path('userindex/', views.userindex, name='userindex'),
+    path('userprofile/', views.userprofile, name='userprofile'),
+    path('login/', views.login, name='login'),
+    path('register/', views.register, name='register'),
+     
+]
+
---
+++ core/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request,'core/index.html')
+def about(request):
+    return render(request,'core/about.html')
+def contact(request):
+    return render(request,'core/contact.html')
+def feedback(request):
+    return render(request,'core/feedback.html')
+def history(request):
+    return render(request,'core/history.html')
+def interview(request):
+    return render(request,'core/interview.html')
+def services(request):
+    return render(request,'core/services.html')
+def userindex(request):
+    return render(request,'core/userindex.html')
+def userprofile(request):
+    return render(request,'core/userprofile.html')
+
+def login(request):
+    return render(request,'core/login.html')
+
+def register(request):
+    return render(request,'core/register.html')
