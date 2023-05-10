cc76fa2593a9479571c81bad40eb5caa95419cf9
usmanjavaid227 <71181962+usmanjavaid227@users.noreply.github.com>
2023-05-10 22:15:45 +0500

    update some video questions and change the table

--- questions/forms.py
+++ questions/forms.py
@@ -1,9 +1,8 @@
 from django import forms
-from .models import Question
+from .models import Question, Answer
 
 class QuestionForm(forms.ModelForm):
     class Meta:
         model = Question
-        fields = ['text']
-        widgets = { 'text': forms.TextInput(attrs={'class': 'form-control'}) }
-
+        fields = ['text', 'video_question']
+        widgets = { 'text': forms.TextInput(attrs={'class': 'form-control'}), 'video_question': forms.FileInput(attrs={'class': 'form-control-file'})}
--- questions/models.py
+++ questions/models.py
@@ -6,6 +6,7 @@ from django.utils import timezone
 
 class Question(models.Model):
     text = models.CharField(max_length=255)
+    video_question = models.FileField(upload_to='Questions/', null=True, verbose_name="")
     created_at = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
