011287207baf1965d1990984a41a19d3718a6808
usmanjavaid227 <71181962+usmanjavaid227@users.noreply.github.com>
2023-05-16 10:49:50 +0500

    analyse's form change a little bit

--- analysis/forms.py
+++ analysis/forms.py
@@ -11,14 +11,3 @@ class AnalysisForm(forms.ModelForm):
             'feedback': forms.Textarea(attrs={'class': 'form-control', 'rows': 5})
         }
         
-        def save(self, commit=True):
-            analysis = super().save(commit=False)
-            if analysis.sentiment_score >= 0.7 and analysis.facial_expression == "confident":
-                analysis.feedback = "Great job! You came across as confident and knowledgeable in your responses. Keep up the good work."
-            elif analysis.sentiment_score < 0.7 and analysis.facial_expression == "neutral":
-                analysis.feedback = "It seems like you could work on being more enthusiastic and engaged in your responses. Practice answering questions with more energy and enthusiasm."
-            else:
-                analysis.feedback = "Your interview performance was good overall. Consider working on improving your responses to some of the more challenging questions."
-            if commit:
-                analysis.save()
-            return analysis
